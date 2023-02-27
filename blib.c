169c85625155a33a938e25ac0ab49ee71fc1a372
chlience <chlience@outlook.com>
2023-02-27 21:10:33 +0800

    finish

--- blib.c
+++ blib.c
@@ -1,11 +1,21 @@
 #include <blib.h>
 
 size_t strlen(const char *s) {
-	panic("please implement");
+	size_t num = 0;
+	while(*s) {
+		++ num;
+		s += sizeof(char);
+	}
+	return num;
 }
 
 char *strcpy(char *dst, const char *src) {
-	panic("please implement");
+	char *res = dst;
+	while(*src) {
+		*dst++ = *src++;
+	}
+	*dst = '\0';
+	return res;
 }
 
 char *strncpy(char *dst, const char *src, size_t n) {
@@ -18,11 +28,27 @@ char *strncpy(char *dst, const char *src, size_t n) {
 }
 
 char *strcat(char *dst, const char *src) {
-	panic("please implement");
+	char *res = dst;
+	while(*dst) ++ dst;
+	while (*src) {
+		*dst++ = *src++;
+	}
+	*dst = '\0';
+	return res;
 }
 
 int strcmp(const char *s1, const char *s2) {
-	panic("please implement");
+	while (1) {
+		if (*s1 != *s2) {
+			return *s1 - *s2;
+		}
+		if (*s1 == 0) {
+			break;
+		}
+		s1++;
+		s2++;
+	}
+	return 0;
 }
 
 int strncmp(const char *s1, const char *s2, size_t n) {
@@ -40,7 +66,11 @@ int strncmp(const char *s1, const char *s2, size_t n) {
 }
 
 void *memset(void *s, int c, size_t n) {
-	panic("please implement");
+	char *ss = (char *)s;
+	for(int i = 0; i < n; ++ i) {
+		*ss = c;
+		++ ss;
+	}
 }
 
 void *memcpy(void *out, const void *in, size_t n) {
@@ -53,5 +83,13 @@ void *memcpy(void *out, const void *in, size_t n) {
 }
 
 int memcmp(const void *s1, const void *s2, size_t n) {
-	panic("please implement");
+	unsigned char *ss1 = (unsigned char *)s1;
+	unsigned char *ss2 = (unsigned char *)s2;
+	for (int i = 0; i < n; i++) {
+		if(*ss1 != *ss2) return *ss1 - *ss2;
+		else if(*ss1 == 0) break;
+		++ ss1;
+		++ ss2;
+	}
+	return 0;
 }
