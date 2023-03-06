76575058127a2ff0a152738a17b37c4559d7ae0a
20374290 <20374290@buaa.edu.cn>
2023-03-06 20:14:16 +0800

    test

--- exam.sh
+++ exam.sh
@@ -1,18 +1,19 @@
-mkdir  mydir
-chmod 777 mydir
+rm -rf ./mydir
+mkdir ./mydir
+chmod 777 ./mydir
 
-echo 2023 > myfile
+echo 2023 > ./myfile
 
-mv -f moveme mydir/
+mv -f moveme ./mydir/
 
-cp -f copyme mydir/copied
+cp -f copyme ./mydir/copied
 
-cat readme
+cat ./readme
 
-gcc bad.c 2>err.txt
+gcc bad.c 2>./err.txt
 
-rm -rf gen
-mkdir gen
+rm -rf ./gen
+mkdir ./gen
 n=10
 if (($# > 0))
 then
@@ -20,6 +21,6 @@ then
 fi
 while (($n > 0))
 do
-	echo "" >gen/"$n.txt"
+	echo "" > ./gen/"$n.txt"
 	let n=n-1
 done
