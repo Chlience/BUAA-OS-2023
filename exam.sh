5aceeff925998376abd1e6f8aaaeffbf35392aa8
20374290 <20374290@buaa.edu.cn>
2023-03-06 20:23:56 +0800

    finish 4

--- exam.sh
+++ exam.sh
@@ -1,26 +1,2 @@
-rm -rf mydir
 mkdir mydir
 chmod 777 mydir
-
-echo 2023 > myfile
-
-mv -f moveme mydir/
-
-cp -f copyme mydir/copied
-
-cat readme
-
-gcc bad.c 2> err.txt
-
-rm -rf gen
-mkdir gen
-n=10
-if (($# > 0))
-then
-    let n=$1
-fi
-while (($n > 0))
-do
-    echo "" > gen/"$n.txt"
-    let n=n-1
-done
