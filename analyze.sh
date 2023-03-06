7541ee2dd455999f8ecafa7078032a9b907e52c2
20374290 <20374290@buaa.edu.cn>
2023-03-06 20:12:14 +0800

    finish no arg

--- analyze.sh
+++ analyze.sh
@@ -3,20 +3,19 @@
 if [ $# -eq 1 ];
 then
     # Your code here. (1/4)
-
+    grep -E 'WARN|ERROR' $1 > bug.txt
 else
     case $2 in
     "--latest")
         # Your code here. (2/4)
-	tail -n 5 $1
+        tail -n 5 $1
     ;;
     "--find")
         # Your code here. (3/4)
-
+        grep -E $3 $1
     ;;
     "--diff")
         # Your code here. (4/4)
-
     ;;
     esac
 fi
