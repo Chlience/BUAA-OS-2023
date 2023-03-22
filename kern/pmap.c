b822048db3ab0269f5ddb7a98de39e673b8da386
20374290 <20374290@buaa.edu.cn>
2023-03-22 15:53:45 +0800

    finish 2.6

--- kern/pmap.c
+++ kern/pmap.c
@@ -182,16 +182,30 @@ static int pgdir_walk(Pde *pgdir, u_long va, int create, Pte **ppte) {
 
 	/* Step 1: Get the corresponding page directory entry. */
 	/* Exercise 2.6: Your code here. (1/3) */
+	pgdir_entryp = pgdir + PDX(va);
 
 	/* Step 2: If the corresponding page table is not existent (valid) and parameter `create`
 	 * is set, create one. Set the permission bits 'PTE_D | PTE_V' for this new page in the
 	 * page directory.
 	 * If failed to allocate a new page (out of memory), return the error. */
 	/* Exercise 2.6: Your code here. (2/3) */
+	if(!(*pgdir) & PTE_V) {
+		if(!create) {
+			*ppte = NULL;
+			return 0;
+		}
+		int ret = page_alloc(&pp);
+		if(ret != 0) {
+			return ret;
+		}
+		(*pgdir_entryp) = page2pa(pp) | PTE_V; // 页表项保存真实地址
+		pp->pp_ref++;
+	}
 
 	/* Step 3: Assign the kernel virtual address of the page table entry to '*ppte'. */
 	/* Exercise 2.6: Your code here. (3/3) */
-
+	pgdir_entryp = (Pde*)PTE_ADDR(*pgdir_entryp) + PTX(va);
+	*ppte = KADDR(pgdir_entryp);
 	return 0;
 }
 
