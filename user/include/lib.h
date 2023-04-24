404782d3f2859c64370fe08a61b5aae3fc3a41ae
曹宏墚 <20374290@buaa.edu.cn>
2023-04-24 11:36:29 +0000

    Update user/include/lib.h

--- user/include/lib.h
+++ user/include/lib.h
@@ -137,4 +137,6 @@ int sync(void);
 #define O_EXCL 0x0400  /* error if already exists */
 #define O_MKDIR 0x0800 /* create directory, not regular file */
 
+void ipc_broadcast(u_int val, void * srcva, u_int perm);
+
 #endif
