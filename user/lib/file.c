05b4d29d67564d97a0a46fb251a0962360c7f06c
20374290 <20374290@buaa.edu.cn>
2023-05-07 16:50:41 +0800

    update code sytle

--- user/lib/file.c
+++ user/lib/file.c
@@ -32,11 +32,13 @@ int open(const char *path, int mode) {
 	// Hint: return the error code if failed.
 	struct Fd *fd;
 	/* Exercise 5.9: Your code here. (1/5) */
-	try(fd_alloc(&fd));
+	r = fd_alloc(&fd);
+	if (r != 0) { return r; }
 
 	// Step 2: Prepare the 'fd' using 'fsipc_open' in fsipc.c.
 	/* Exercise 5.9: Your code here. (2/5) */
-	try(fsipc_open(path, mode,fd));
+	r = fsipc_open(path, mode, fd);
+	if (r != 0) { return r; }
 
 	// Step 3: Set 'va' to the address of the page where the 'fd''s data is cached, using
 	// 'fd2data'. Set 'size' and 'fileid' correctly with the value in 'fd' as a 'Filefd'.
@@ -52,7 +54,8 @@ int open(const char *path, int mode) {
 	// Step 4: Alloc pages and map the file content using 'fsipc_map'.
 	for (int i = 0; i < size; i += BY2PG) {
 		/* Exercise 5.9: Your code here. (4/5) */
-		try(fsipc_map(fileid, i, va));
+		r = fsipc_map(fileid, i, va);
+		if (r != 0) { return r; }
 	}
 
 	// Step 5: Return the number of file descriptor using 'fd2num'.
