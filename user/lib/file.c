0401880ef05e37e500a9fd21978fbc203a2ae5be
20374290 <20374290@buaa.edu.cn>
2023-05-21 21:39:42 +0800

    fix open() in file.c

--- user/lib/file.c
+++ user/lib/file.c
@@ -54,7 +54,7 @@ int open(const char *path, int mode) {
 	// Step 4: Alloc pages and map the file content using 'fsipc_map'.
 	for (int i = 0; i < size; i += BY2PG) {
 		/* Exercise 5.9: Your code here. (4/5) */
-		r = fsipc_map(fileid, i, va);
+		r = fsipc_map(fileid, i, va + i);
 		if (r != 0) { return r; }
 	}
 
